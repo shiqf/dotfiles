e8db716675180e6eeb1a40fc208d58018cccccd1
shiqf <jiyuuinchina@hotmail.com>
2019-10-24 10:01:26 +0800

    fix

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -45,10 +45,14 @@ flags = [
 '/usr/include',
 '-isystem',
 '/usr/local/include',
+# '-isystem',
+# '/Library/Developer/CommandLineTools/usr/bin/../include/c++/v1',
 '-isystem',
-'/Library/Developer/CommandLineTools/usr/include/c++/v1',
+'/Library/Developer/CommandLineTools/usr/include',
 '-isystem',
-'/Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/usr/include',
+'/Library/Developer/CommandLineTools/usr/lib/clang/11.0.0/include',
+'-isystem',
+'/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include',
 ]
 
 
